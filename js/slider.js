fa9f20f56f144f0c7052218ed0c1480a23356866
mixtuti桜餅 <88471532+mixtuti@users.noreply.github.com>
2023-07-13 12:43:16 +0900

    Add files via upload

--- js/slider.js
+++ js/slider.js
@@ -2,7 +2,7 @@ $('.slider').slick({
     arrows: false, //左右の矢印はなし
 	autoplay: true, //自動的に動き出すか。初期値はfalse。
 	autoplaySpeed: 0, //自動的に動き出す待ち時間。初期値は3000ですが今回の見せ方では0
-	speed: 30000, //スライドのスピード。初期値は300。
+	speed: 15000, //スライドのスピード。初期値は300。
 	infinite: true, //スライドをループさせるかどうか。初期値はtrue。
 	pauseOnHover: false, //オンマウスでスライドを一時停止させるかどうか。初期値はtrue。
 	pauseOnFocus: false, //フォーカスした際にスライドを一時停止させるかどうか。初期値はtrue。
